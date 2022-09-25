e14a9b0e5123fa9646cef6ea0270cf1f9d3c9cf2
Frank Blankenburg <fblankenburg@gom.com>
2022-09-25 20:26:21 +0200

    Initial documentation commit

---
+++ doc/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Python API documentation'
+copyright = '2022, Carl Zeiss GOM Metrology GmbH'
+author = 'Carl Zeiss GOM Metrology GmbH'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser', 'sphinx_rtd_theme']
+source_suffix = ['.rst', '.md']
+
+templates_path = ['_templates']
+exclude_patterns = ['README.md']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
---
+++ doc/index.rst
@@ -0,0 +1,9 @@
+Python API documentation
+========================
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents
+
+   howtos/adding_workspaces_to_packages/adding_workspaces_to_packages
+   howtos/using_vscode_editor/using_vscode_editor
