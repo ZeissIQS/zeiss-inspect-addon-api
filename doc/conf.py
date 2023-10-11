bc1aa8999ed38f34f06ad65e33f9c1841293c22d
mprinkezs <131883374+mprinkezs@users.noreply.github.com>
2023-10-11 12:27:39 +0200

    Fixed html_baseurl

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ myst_heading_anchors = 4
 
 # -- Options for sitemap -----------------------------------------------------
 # https://sphinx-sitemap.readthedocs.io/en/latest/getting-started.html
-html_baseurl = 'https://zeissiqs.github.io/2023/'
+html_baseurl = 'https://zeissiqs.github.io/zeiss-inspect-addon-api/2023/'
 sitemap_url_scheme = "{link}"
 
 # -- Options for HTML output -------------------------------------------------
